bf6f09f5a687195d2187fcd9f96ef34b33b1d2c4
Ter2004 <Kittinantantajina.123@gmail.com>
2024-10-15 17:41:12 +0700

    footer.tsx

---
+++ Homework/music store/components/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Main.css';
+
+type Props = {}
+
+export default function Footer({}: Props) {
+  return (
+    <footer>
+        <div className='footer'>
+        © 2024 [Music Store ]. All rights reserved.
+      
+        </div>
+    </footer>
+  )
+}
